efacb4b9cd0f1ccc1160cabf3285fe3f6fae26f8
Sagar malik <66472235+Sagarmalik4697@users.noreply.github.com>
2021-08-06 23:47:59 +0530

    middleElementInStack

---
+++ DSA Programs/Stack and Queues/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+* Representation of the stack data structure that supports
+findMiddle() in O(1) time. The Stack is implemented
+using Doubly Linked List. It maintains pointer to head
+node, pointer to middle node and count of nodes */
